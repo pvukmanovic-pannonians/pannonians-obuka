ee15b940edd94ad20f8865bd9ae6512c764a9426
Marina Jokic <marina.jokic@pannonians.com>
2022-11-14 18:58:48 +0100

    marina/copy_paste_js

---
+++ marina/comments/index.js
@@ -0,0 +1,9 @@
+// Ovo je komentar koji je u jednoj liniji
+console.log("Hello World")
+
+/* 
+    Ovo je komentar
+    Sad koji moze da ime vise
+    linija u sebi
+*/
+console.log("Hello World again")
