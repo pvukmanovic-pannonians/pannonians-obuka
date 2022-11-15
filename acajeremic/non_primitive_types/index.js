9b041015e49fcda7d5009b6720de3176f0a703ae
Aleksandar Jeremic <acajeremic@gmail.com>
2022-11-15 22:10:17 +0100

    dodao strukturu za js

---
+++ acajeremic/non_primitive_types/index.js
@@ -0,0 +1,21 @@
+console.log({}); // object
+
+console.log({
+    ime: "Aleksandar",
+    godine: 34,
+    gde_sam_rodjen: {
+        mesto: "Sremska Mitrovica"
+    },
+    da_li_predajemo_JS: true
+}.da_li_predajemo_JS);
+
+console.log(new Object()); // object
+
+
+console.log([]); // array
+console.log([
+    "seno",
+    "jaje",
+    "petao",
+    "kokoska"
+]);
