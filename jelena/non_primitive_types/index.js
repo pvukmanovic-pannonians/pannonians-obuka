cb31ad6ee34daf271dbc72acd7d43eaa008c28fc
stevanovicj <stevanovic.jelena@outlook.com>
2022-11-15 15:11:26 +0100

    copy_paste_js

---
+++ jelena/non_primitive_types/index.js
@@ -0,0 +1,21 @@
+ console.log({}); //object
+ console.log([]); //array
+
+ console.log({
+    ime: "Jelena",
+    godine: 7,
+    mesto_rodjenja:{
+        mesto: "Novi Sad"
+    },
+    da_li_predajemo_JS: true
+ }.da_li_predajemo_JS);
+
+ console.log(new Object()); //object
+
+ console.log([]); //array
+ console.log([
+    "petar",
+    "aleksandra",
+    "marina",
+    "jovana"
+ ]);
