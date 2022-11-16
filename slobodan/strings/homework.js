8f3cf3dc49e2c7b9afdfce851d00d1c0e0ddde11
Slobodan Novakovic <slobodan.ucenje@gmail.com>
2022-11-16 19:09:50 +0100

    Strings homework done

---
+++ slobodan/strings/homework.js
@@ -0,0 +1,25 @@
+
+const firstname = "slobodan";
+
+console.log(firstname); 
+
+const lastname ="novakovic";
+
+console.log(lastname);
+
+const fullname = firstname + " " + lastname;
+
+console.log(fullname);
+
+console.log(fullname.toUpperCase());
+
+console.log(fullname.split(" "));
+
+console.log(`${firstname} ${firstname.length}`);
+
+console.log(`${lastname} ${lastname.length}`);
+
+console.log (`${fullname} ${fullname.split(" ").join("").length}`)
+
+
+
