33278d8b85fb6b93a1696f9d112c42d97aec605c
Dusan Gajic <dusan.gajic@pannonians.com>
2022-11-16 14:12:34 +0100

    created data types folder structure, along with the index js file for each type

---
+++ dusan/comments/index.js
@@ -0,0 +1,19 @@
+console.log("Playing around with the comments")
+
+// This is just a one-line comment!
+
+const firstNumber = 7
+var secondNumber
+let result
+result = "The result is"
+
+/* 
+This is just a one-line comment.
+Comments are used to clarify the code.
+They are not printed or executed!
+*/
+
+
+
+
+console.log(result + ":" + " " + (firstNumber * (secondNumber= 3+2)) + " /// I do not want to print the comments!")
