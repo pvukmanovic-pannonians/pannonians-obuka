e5c0cbeb1ad9e6039a3ff5a5983377c173b50f4e
Milana Dokic <milana.dokic.021@gmail.com>
2022-11-17 16:57:48 +0100

    Uradjen domaci zadatak.

---
+++ milana/non_primitive_types/index.js
@@ -0,0 +1,21 @@
+console.log({}); // object
+
+console.log({
+    ime: "Petar",
+    godine: 19,
+    gde_sam_rodjen: {
+        mesto: "Novi Sad"
+    },
+    da_li_predajemo_JS: true
+}.da_li_predajemo_JS);
+
+console.log(new Object()); // object
+
+
+console.log([]); // array
+console.log([
+    "petar",
+    "aleksandar",
+    "marina",
+    "dusan"
+]);
