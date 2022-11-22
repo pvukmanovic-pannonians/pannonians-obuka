accf7231bf7a16b25200a29ab09096bc322acf8d
Ognjen Dokic <ognjen.dokic@noubis.com>
2022-11-22 15:38:12 +0100

    Domaci iz funkcija

---
+++ milana/functions/homework.js
@@ -0,0 +1,35 @@
+function sabiranje (a,b) {
+    let rezultat = a + b
+    console.log("rezultat sabiranja", rezultat);
+}
+
+function oduzimanje(a,b) { 
+    let rezultat = a - b
+    console.log("rezultat oduzimanja", rezultat);
+}
+
+function deljenje(a,b) {
+    let rezultat = a / b
+    console.log("rezultat deljenja", rezultat);
+}
+
+function mnozenje(a,b) {
+    console.log("rezultat mnozenja", a * b);
+}
+
+function kvadrat(a) {
+    console.log("kvadrat broja", a*a);
+}
+
+function moduo(a,b) {
+    console.log("rezultat modua", a % b);
+}
+
+sabiranje(3,7) 
+oduzimanje(8,4)
+deljenje(63,9)
+mnozenje(6,8)
+kvadrat(7)
+moduo(10,3)
+moduo(10,4)
+
