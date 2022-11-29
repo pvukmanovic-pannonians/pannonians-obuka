36df790aa8e4d1cb6b0ed346348efdd28c754abd
Aleksandra Vukasinovic <azobenica@gmail.com>
2022-11-29 14:01:28 +0100

    petlje

---
+++ aleksandra/loops/index.js
@@ -0,0 +1,43 @@
+let str = "";
+
+for (let i = 0; i < 9; i++) {
+  str = str + i;
+}
+
+// console.log(str);
+
+const objekat = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+const niz = [1, 2, 3, 4, 5];
+
+// for (let qwe in objekat) {
+//   console.log("key is " + qwe + ", and value is " + objekat[qwe]);
+// }
+
+// console.log(qwe);
+
+// for (let qwe of niz) {
+//   console.log(qwe);
+// }
+
+// niz.forEach((jedan_u_nizu, index) => {
+//   console.log(jedan_u_nizu, index);
+// });
+
+// let n = 0;
+
+// while (n < 30) {
+//   n++;
+//   if (n == 20) {
+//     break;
+//   }
+//   console.log(n);
+// }
+
+// console.log(n);
+// expected output: 3
+console.log("Petlje :)");
