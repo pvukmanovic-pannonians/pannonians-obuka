9096afd98d01dff10ef0dbbb449b3645ebbf0253
Petar Vukmanovic <petar.vukmanovic@pannonians.com>
2022-12-09 18:04:52 +0100

    dino kaki

--- petar/hello-world/src/App.js
+++ petar/hello-world/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <TextText />
       </header>
     </div>
   );
 }
 
 export default App;
+
+const TextText = () => {
+  const text = "Hello World";
+
+  const [textKomponente, setTextKomponente] = useState(text);
+
+  return (
+    <>
+      <h1>{textKomponente}</h1>
+      <img src="https://media1.giphy.com/media/lTigJvWJwtlchDTXQY/giphy.gif" />
+      <button
+        onClick={() => {
+          setTextKomponente(textKomponente === text ? "Promenjen text" : text);
+        }}
+        type="button"
+        style={{ fontSize: 48 }}
+      >
+        Change text
+      </button>
+    </>
+  );
+};
