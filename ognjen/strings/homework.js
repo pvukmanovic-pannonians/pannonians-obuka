8a4385b2ac9263caf9649bb676af1dcfe825fe8a
Ognjen Dokic <ognjen.dokic@noubis.com>
2022-12-11 12:13:18 +0100

    Domaci iz stringova

---
+++ ognjen/strings/homework.js
@@ -0,0 +1,7 @@
+const firstName = "ognjen"
+const lastName = "dokic"
+
+const firstNameCapital = firstName[0].toUpperCase() + firstName.substring(1)
+const lastNameCapital = lastName[0].toUpperCase() + lastName.substring(1)
+
+console.log('full name: ', firstNameCapital + ' ' + lastNameCapital);
