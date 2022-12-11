121e9695c37cb765d82a7772b688350a7c1c9b55
Ognjen Dokic <ognjen.dokic@noubis.com>
2022-12-11 12:22:01 +0100

    Domaci iz funkcija

---
+++ ognjen/functions/homework.js
@@ -0,0 +1,30 @@
+const add = (param1, param2) => {
+    console.log('adding result: ', param1 + param2);
+}
+
+const subs = (param1, param2) => {
+    console.log('substract result: ', param1 - param2);
+}
+
+const multiply = (param1, param2) => {
+    console.log('multiplication result: ', param1 * param2);
+}
+
+const divide = (param1, param2) => {
+    console.log('division result: ', param1 / param2);
+}
+
+const square = (param1) => {
+    console.log('square result: ', param1 * param1);
+}
+
+const moduo = (param1, param2) => {
+    console.log('moduo result: ', param1 % param2);
+}
+
+add(5, 17)
+subs(12, 4)
+multiply(3,7)
+divide(64, 8)
+square(4)
+moduo(72, 7)
