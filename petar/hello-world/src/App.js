5cab2068fd34c1502a5abe61965a7b201794c0aa
Petar Vukmanovic <petar.vukmanovic@pannonians.com>
2022-12-12 18:48:17 +0100

    props vs state

--- petar/hello-world/src/App.js
+++ petar/hello-world/src/App.js
@@ -1,36 +1,30 @@
-import { useState } from "react";
+import Dino from "./components/TextText";
 import "./App.css";
+import { Pozadina as Background, alertiranje } from "./components/Pozadina";
 
 function App() {
+  const text = "Ovde neki text prosledjen iz parent komponente";
+
+  const funkcija = () => {
+    alert("Pozvana neka funkcija");
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <TextText />
+        <Dino callback={funkcija} paragraph={text} />
+        <Dino callback={alertiranje} paragraph="asdasdasdds" />
+        <Dino callback={funkcija} paragraph={12312321} />
+        <Dino callback={funkcija} paragraph={12312321} />
+
+        <Background>
+          <p>qweqwe</p>
+          <p>qweqwe</p>
+          <p>qweqwe</p>
+        </Background>
       </header>
     </div>
   );
 }
 
 export default App;
-
-const TextText = () => {
-  const text = "Hello World";
-
-  const [textKomponente, setTextKomponente] = useState(text);
-
-  return (
-    <>
-      <h1>{textKomponente}</h1>
-      <img src="https://media1.giphy.com/media/lTigJvWJwtlchDTXQY/giphy.gif" />
-      <button
-        onClick={() => {
-          setTextKomponente(textKomponente === text ? "Promenjen text" : text);
-        }}
-        type="button"
-        style={{ fontSize: 48 }}
-      >
-        Change text
-      </button>
-    </>
-  );
-};
