88751e47de71db354ed071cc1acf8e48b31fab04
Petar Vukmanovic <petar.vukmanovic@pannonians.com>
2022-12-14 18:06:23 +0100

    form submissions

--- petar/hello-world/src/App.js
+++ petar/hello-world/src/App.js
@@ -1,33 +1,58 @@
-import Dino from "./components/TextText";
+import { useState, useEffect } from "react";
 import "./App.css";
-import { Pozadina as Background } from "./components/Pozadina";
-import { useState } from "react";
+
+function validate_mail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+    return true;
+  }
+  return false;
+}
 
 function App() {
-  const text = "Ovde neki text prosledjen iz parent komponente";
-  const [toggle, setToggle] = useState(true);
+  const [inputValue, setInputValue] = useState();
+  const [submittable, setSubmittable] = useState(false);
+  const [isMailValid, setIsMailValid] = useState(true);
 
-  const funkcija = () => {
-    // alert("Pozvana neka funkcija");
-  };
+  useEffect(() => {
+    if (validate_mail(inputValue)) {
+      setSubmittable(true);
+      setIsMailValid(true);
+    } else {
+      setSubmittable(false);
+      setIsMailValid(false);
+    }
+  }, [inputValue]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <button
-          onClick={() => {
-            setToggle(!toggle);
+        <h1>Two way data binding</h1>
+        <form
+          style={{ display: "flex", flexDirection: "column" }}
+          onSubmit={(event) => {
+            event.preventDefault();
+            console.log("moja mail adresa je: " + inputValue);
           }}
         >
-          change toggle
-        </button>
-        {toggle ? (
-          <Dino callback={funkcija} paragraph={text} />
-        ) : (
-          <Background>
-            <p>qweqwe</p>
-          </Background>
-        )}
+          <input
+            type="text"
+            value={inputValue}
+            onChange={(event) => {
+              setInputValue(event.target.value);
+            }}
+            style={{ fontSize: 40 }}
+          />
+          {!isMailValid && inputValue && inputValue.length !== 0 && (
+            <span style={{ color: "red" }}>Your mail is not valid</span>
+          )}
+          <button
+            disabled={!submittable}
+            style={{ fontSize: 30 }}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
       </header>
     </div>
   );
