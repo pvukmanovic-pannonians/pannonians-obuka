794b052cbec28359ff0a312a8caf12660d520f3a
Petar Vukmanovic <petar.vukmanovic@pannonians.com>
2022-12-15 18:00:45 +0100

    useCallback i useMemo

--- petar/hello-world/src/App.js
+++ petar/hello-world/src/App.js
@@ -1,61 +1,89 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useMemo, useCallback } from "react";
 import "./App.css";
 
-function validate_mail(mail) {
-  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
-    return true;
-  }
-  return false;
-}
+// function validate_mail(mail) {
+//   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+//     return true;
+//   }
+//   return false;
+// }
 
 function App() {
-  const [inputValue, setInputValue] = useState();
-  const [submittable, setSubmittable] = useState(false);
-  const [isMailValid, setIsMailValid] = useState(true);
+  const inputField = useRef();
+  const [brojevi, setBrojevi] = useState([]);
+  const [nesto, setNesto] = useState(false);
 
-  useEffect(() => {
-    if (validate_mail(inputValue)) {
-      setSubmittable(true);
-      setIsMailValid(true);
-    } else {
-      setSubmittable(false);
-      setIsMailValid(false);
-    }
-  }, [inputValue]);
+  const sortirano = useMemo(() => {
+    console.log("memoizovana funkcija se izvrsava", brojevi);
+    return [...brojevi].sort((a, b) => a - b);
+  }, [brojevi]);
+
+  const sortiranoKrozCallback = useCallback(() => {
+    console.log("useCallback se izvrsava", brojevi);
+    return [...brojevi].sort((a, b) => a - b);
+  }, [brojevi]);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Two way data binding</h1>
+        <h1>Memoization</h1>
         <form
-          style={{ display: "flex", flexDirection: "column" }}
-          onSubmit={(event) => {
-            event.preventDefault();
-            console.log("moja mail adresa je: " + inputValue);
+          onSubmit={(e) => {
+            e.preventDefault();
+            setBrojevi([...brojevi, parseInt(inputField.current.value)]);
           }}
         >
-          <input
-            type="text"
-            value={inputValue}
-            onChange={(event) => {
-              setInputValue(event.target.value);
-            }}
-            style={{ fontSize: 40 }}
-          />
-          {!isMailValid && inputValue && inputValue.length !== 0 && (
-            <span style={{ color: "red" }}>Your mail is not valid</span>
-          )}
-          <button
-            disabled={!submittable}
-            style={{ fontSize: 30 }}
-            type="submit"
-          >
-            Submit
-          </button>
+          <input ref={inputField} type="text" />
+          <input type="submit" />
         </form>
+        {nesto ? "true" : "false"}
+        {sortirano.map((broj) => (
+          <div key={broj}>{broj}</div>
+        ))}
+        <button
+          onClick={() => {
+            setNesto(!nesto);
+          }}
+        >
+          Promeni nesto
+        </button>
+        <button
+          onClick={() => {
+            console.log(sortiranoKrozCallback());
+          }}
+        >
+          pozovi callback
+        </button>
       </header>
     </div>
   );
 }
 
 export default App;
+
+// function Forma({ setInputValue, isMailValid, inputValue, submittable }) {
+//   return (
+//     <form
+//       style={{ display: "flex", flexDirection: "column" }}
+//       onSubmit={(event) => {
+//         event.preventDefault();
+//         console.log("moja mail adresa je: " + inputValue);
+//       }}
+//     >
+//       <input
+//         type="text"
+//         value={inputValue}
+//         onChange={(event) => {
+//           setInputValue(event.target.value);
+//         }}
+//         style={{ fontSize: 40 }}
+//       />
+//       {!isMailValid && inputValue && inputValue.length !== 0 && (
+//         <span style={{ color: "red" }}>Your mail is not valid</span>
+//       )}
+//       <button disabled={!submittable} style={{ fontSize: 30 }} type="submit">
+//         Submit
+//       </button>
+//     </form>
+//   );
+// }
