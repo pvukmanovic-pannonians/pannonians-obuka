5e8b6a27bd1da3b322329d9157e4f1b3886b9c39
Ognjen Dokic <ognjen.dokic@noubis.com>
2022-12-21 15:08:53 +0100

    Catching up with react

---
+++ ognjen/hello-world/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { useEffect, useState } from 'react';
+import Layout from './layouts/main';
+
+function App() {
+  return (
+    <Layout></Layout>
+  );
+}
+
+export default App;
+
+
+
