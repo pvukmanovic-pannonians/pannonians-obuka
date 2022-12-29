568e2418a60d1f70b10bc7ac885948eb71159897
Ognjen Dokic <ognjen.dokic@noubis.com>
2022-12-29 14:00:53 +0100

    Removed style and replaced it with class

--- ognjen/pay-pal/src/components/FifthSection.jsx
+++ ognjen/pay-pal/src/components/FifthSection.jsx
@@ -1,7 +1,7 @@
 import Button from 'react-bootstrap/Button';  
 export default function FifthSection({children}) {
     return (
-        <div className='d-flex flex-column align-items-center py-5' style={{backgroundColor: '#f7f9fa'}}>
+        <div className='d-flex flex-column align-items-center py-5 section5'>
             <p style={{fontSize: '28px'}}>Sign up and get started.</p>
             <Button className='mt-5 py-3 px-5' style={{borderRadius: '30px'}} variant="primary">Get Started</Button>{' '} 
         </div>
