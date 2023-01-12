55b8c536db52f721e9d47cba954420249e9f5b6f
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-12 17:42:26 +0100

    Added about page content

--- ognjen/ogijev-blog/src/pages/About.jsx
+++ ognjen/ogijev-blog/src/pages/About.jsx
@@ -1,7 +1,14 @@
 export default function About() {
     return (
-        <div>
-            <p>About Us</p> 
+        <div style={{display: 'flex', flexDirection: 'column', padding: '0 20px', alignItems: 'center', justifyContent: 'center'}}>
+            <h1>About</h1>
+            <p style={{maxWidth: '800px'}}>Makers, breakers, earth shakers. We are page that brings you all inclusive information on fields of humanity.
+                We care not for discrimination, defamation and other foolish things. All are welcome to write articles on our page.
+                Welcome and let those whom you dislike feel the sting of your pen.
+            </p>
+            <div style={{height: '450px', width: '450px'}}>
+                <img style={{maxWidth: '100%', maxHeight: '100%'}} src="https://creakyjoints.org/wp-content/uploads/2020/07/0220_Chronic_Pain_Speedometer_Logo.jpg" alt="" />
+            </div>
         </div>
     )
 }
--- ognjen/ogijev-blog/src/pages/Home.jsx
+++ ognjen/ogijev-blog/src/pages/Home.jsx
@@ -2,6 +2,7 @@ export default function Home ({child}) {
     return (
         <div>
             <p>Home Page</p>
+            <p>Nothing for you here. Go to blog page. ^^</p>
         </div>
     )
 }
