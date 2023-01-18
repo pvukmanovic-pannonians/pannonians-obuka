b37af823fc56fdd7b8769e80192dec903da02c7f
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-18 11:46:43 +0100

    Adding styles to navigation bar

--- ognjen/prodavnica/src/App.js
+++ ognjen/prodavnica/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import MainLayout from './layout/MainLayout';
 import Routes from './Routes';
+import NavigatioBar from './components/NavigationBar'
 
 function App() {
   return (
+    <>
+      <NavigatioBar />
       <MainLayout>
         <Routes></Routes>
       </MainLayout>
+    </>   
   );
 }
 
