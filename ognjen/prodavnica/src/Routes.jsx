c665a2a36caff0f7d33ad433036b2f3600dde52f
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-18 11:26:46 +0100

    Created login page and added it to routes

--- ognjen/prodavnica/src/Routes.jsx
+++ ognjen/prodavnica/src/Routes.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes as BaseRoutes } from 'react-router';
 import LandingPage from './pages/LandingPage';
+import Login from './pages/Login';
 
 export default function Routes () {
     return (
         <BaseRoutes>
             <Route path="/" element={<LandingPage />}>Home</Route>
-            <Route path='/login'></Route>
+            <Route path='/login' element={<Login />}></Route>
             <Route path='/about'></Route>
         </BaseRoutes>
     )
