c43ec8c2cda7253dd47dc1241857c11d36c36e7c
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-18 11:18:26 +0100

    Created landing page and added to routes

--- ognjen/prodavnica/src/Routes.jsx
+++ ognjen/prodavnica/src/Routes.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes as BaseRoutes } from 'react-router'
+import LandingPage from './pages/LandingPage'
 export default function Routes () {
     return (
         <BaseRoutes>
-            <Route path='/'>Home</Route>
+            <Route path='/' element={<LandingPage />}>Home</Route>
             <Route path='/login'></Route>
             <Route path='/about'></Route>
         </BaseRoutes>
