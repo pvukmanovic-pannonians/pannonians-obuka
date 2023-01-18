c0bef7d94276ef26f7f34d8c792787ac57ce300c
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-18 14:03:17 +0100

    Added logic for loging in and calling post request to server

--- ognjen/prodavnica/src/components/LoginForm.jsx
+++ ognjen/prodavnica/src/components/LoginForm.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import { useSelector } from "react-redux"
+import axios from 'axios'
+
 export default function LoginForm () {
     const [errorMessages, setErrorMessage] = useState({});
     const [isSubmitted, setIsSubmitted] = useState(false);
+    const [userName, setUserName] = useState()
+    const [userPassword, setUserPassword] = useState()
 
     const renderErrorMessage = (name) => {
         name === errorMessages.name && (<div className="error">{errorMessages.message}</div>)
     }
-    const usersDatabase = []
+    const usersDatabase = useSelector(state => state.users.users)
     const errors = {
         uname: "invalid username",
         pass: "invalid password"
@@ -14,11 +19,16 @@ export default function LoginForm () {
     const handleLogin = async (event) => {
         event.preventDefault()
 
-        const { uname, pass } = document.forms[0]
+        const { uname, password } = document.forms[0]
+        console.log('event', event.target);
         const userData = usersDatabase.find(user => user.username === uname.value)
+        await axios.post('https://dummyjson.com/auth/login', {
+            username: userName,
+            password: userPassword
+        })
         
         if (userData) {
-            if (userData.password !== pass.value) {
+            if (userData.password !== password.value) {
                 setErrorMessage({name: 'password', message: errors.pass})
             } else {
                 setIsSubmitted(true)
@@ -33,12 +43,12 @@ export default function LoginForm () {
             {isSubmitted ? <div>User succesfully loged in</div> :  <form className="loginForm" onSubmit={handleLogin}>
                 <div className="formSection">
                     <label>Username</label>
-                    <input type="text" name="uname" className="formInputField"/>
+                    <input type="text" name="uname" value={userName} onChange={e => setUserName(e.target.value)} className="formInputField" required/>
                     {renderErrorMessage('uname')}
                 </div>
                 <div className="formSection">
                     <label>Password</label>
-                    <input type="password" name="password" className="formInputField" />
+                    <input type="password" name="password" value={userPassword} onChange={e => setUserPassword(e.target.value)} className="formInputField"  required/>
                     {renderErrorMessage('password')}
                 </div>
                 <div className="">
