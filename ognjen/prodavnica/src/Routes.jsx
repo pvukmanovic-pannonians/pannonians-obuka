25789d12e40963e497d0bb04433852dc8a810222
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-18 11:28:48 +0100

    removed hardcoded part from routes jsx

--- ognjen/prodavnica/src/Routes.jsx
+++ ognjen/prodavnica/src/Routes.jsx
@@ -6,7 +6,7 @@ import Login from './pages/Login';
 export default function Routes () {
     return (
         <BaseRoutes>
-            <Route path="/" element={<LandingPage />}>Home</Route>
+            <Route path="/" element={<LandingPage />}></Route>
             <Route path='/login' element={<Login />}></Route>
             <Route path='/about' element={<About />}></Route>
         </BaseRoutes>
