203e8a15f03011d753a0a44b3805f02e139e5617
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-18 11:25:09 +0100

    Added Browser router to index js

--- ognjen/prodavnica/src/Routes.jsx
+++ ognjen/prodavnica/src/Routes.jsx
@@ -1,9 +1,10 @@
-import { Route, Routes as BaseRoutes } from 'react-router'
-import LandingPage from './pages/LandingPage'
+import { Route, Routes as BaseRoutes } from 'react-router';
+import LandingPage from './pages/LandingPage';
+
 export default function Routes () {
     return (
         <BaseRoutes>
-            <Route path='/' element={<LandingPage />}>Home</Route>
+            <Route path="/" element={<LandingPage />}>Home</Route>
             <Route path='/login'></Route>
             <Route path='/about'></Route>
         </BaseRoutes>
--- ognjen/prodavnica/src/pages/LandingPage.jsx
+++ ognjen/prodavnica/src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 export default function LandingPage () {
     return (
         <div>
-            <h1>Landing Page</h1>
+            <h1>This is Landing Page</h1>
         </div>
     )
 }
