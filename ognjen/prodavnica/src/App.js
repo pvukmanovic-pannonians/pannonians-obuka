1b226cd86361a95a3895ca53323ed1be476d1902
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-01-19 18:04:18 +0100

    changes, changes

--- ognjen/prodavnica/src/App.js
+++ ognjen/prodavnica/src/App.js
@@ -14,7 +14,6 @@ function App() {
           <Routes></Routes>
         </MainLayout>
       </Provider>
-   
     </>   
   );
 }
--- ognjen/prodavnica/src/components/LoginForm.jsx
+++ ognjen/prodavnica/src/components/LoginForm.jsx
@@ -23,14 +23,12 @@ export default function LoginForm () {
         event.preventDefault()
 
         const { uname, password } = document.forms[0]
-        console.log('event', event.target);
         const userData = usersDatabase.find(user => user.username === uname.value)
-        // dispatch(currentUser(userData))
        const data =  await axios.post('https://dummyjson.com/auth/login', {
             username: userName,
             password: userPassword
         })
-        dispatch(currentUser(data))
+        dispatch(currentUser(data.data))
         
         if (userData) {
             if (userData.password !== password.value) {
