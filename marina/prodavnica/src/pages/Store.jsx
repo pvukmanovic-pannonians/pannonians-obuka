a83ad57f31a380652d0b9bba60865fcc4fbc34cb
Marina Jokic <marina.jokic@pannonians.com>
2023-01-30 12:16:14 +0100

    added filter by category

--- marina/prodavnica/src/pages/Store.jsx
+++ marina/prodavnica/src/pages/Store.jsx
@@ -4,8 +4,9 @@ import { endpoints } from "../http/client";
 
 export default function Store() {
   const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
 
-  useEffect(() => {
+useEffect(() => {
     axios.get(endpoints.SHOW).then((response) => {
       console.log("lista", response.data.products);
       const products = response.data.products;
@@ -13,35 +14,63 @@ export default function Store() {
     });
   }, []);
 
+  const categories = [
+    "smartphones",
+    "laptops",
+    "fragrances",
+    "skincare",
+    "groceries",
+    "home-decoration",
+  ];
+
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
   return (
     <div className="product-container">
-      {products.map((product) => (
-        <div className="product" key={product.id}>
-          <h2>{product.title}</h2>
-          <p>{product.description}</p>
-          <p>Price: ${product.price}</p>
-          <p>Discount Percentage: {product.discountPercentage}%</p>
-          <p>Rating: {product.rating}</p>
-          <p>Stock: {product.stock}</p>
-          <p>Brand: {product.brand}</p>
-          <p>Category: {product.category}</p>
-          <img
-            src={product.thumbnail}
-            alt="product thumbnail"
-            style={{ maxWidth: "200px" }}
-          />
-          <div className="images">
-            {product.images.map((image, index) => (
-              <img
-                key={index}
-                src={image}
-                style={{ maxWidth: "50px" }}
-                alt={product.title}
-              />
-            ))}
+      <div className="filter-container">
+        <label htmlFor="category">Filter by category: </label>
+        <select id="category" onChange={handleCategoryChange}>
+          <option value="">All</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      {products
+        .filter((product) =>
+          selectedCategory ? product.category === selectedCategory : true
+        )
+        .map((product) => (
+          <div className="product" key={product.id}>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <p>Price: ${product.price}</p>
+            <p>Discount Percentage: {product.discountPercentage}%</p>
+            <p>Rating: {product.rating}</p>
+            <p>Stock: {product.stock}</p>
+            <p>Brand: {product.brand}</p>
+            <p>Category: {product.category}</p>
+            <img
+              src={product.thumbnail}
+              alt="product thumbnail"
+              style={{ maxWidth: "200px" }}
+            />
+            <div className="images">
+              {product.images.map((image, index) => (
+                <img
+                  key={index}
+                  src={image}
+                  style={{ maxWidth: "50px" }}
+                  alt={product.title}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   );
 }
