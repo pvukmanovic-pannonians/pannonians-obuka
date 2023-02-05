ee07a0a22332a23d71c647a5b0bd7bc1403d1226
Ognjen Dokic <ognjen.dokic@noubis.com>
2023-02-05 12:30:16 +0100

    Disabled store link unless user is loged in

--- ognjen/prodavnica/src/components/NavigationBar.jsx
+++ ognjen/prodavnica/src/components/NavigationBar.jsx
@@ -1,14 +1,16 @@
 // import { useSelector } from "react-redux"
+import { useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 export default function NavigationBar () {
     // const user = useSelector(state => state.users.user)
+    const user = useSelector(state => state.users.user)
     return (
         <nav className="navigationBar">
             <ul>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/login'>Login</Link></li>
-                <li><Link to="/store">Store</Link></li>
+                <li><Link to="/store" style={!user.id ? {pointerEvents: 'none'} : {}}>Store</Link></li>
                 {/* {user && (<div className="userProfile">
                     <div className="userAvatar">
                         <img src={user.data.image} alt="" className="avatarImage" />
